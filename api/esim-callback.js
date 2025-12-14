7485722c554b98a2fa71aa48f6fa70499d4d2d80
ezfones <matt_vairy@yahoo.co.uk>
2025-12-14 21:08:24 +0000

    Delete api/esim-callback.js

--- api/esim-callback.js
+++
@@ -1,9 +0,0 @@
-﻿export default async function handler(req, res) {
-  const body = req.body || {};
-
-  // eSIM GO callback receiver
-  res.status(200).json({
-    ok: true,
-    receivedCallback: body
-  });
-}
