0516095e0feeea7d1a53f1d4bac3ae440043a601
ezfones <matt_vairy@yahoo.co.uk>
2025-12-18 08:18:39 +0000

    Update ping.js

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,7 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: "API is working",
-    timestamp: new Date().toISOString()
-  });
+  res.status(200).json({ ok: true, message: "ping ok", ts: new Date().toISOString() });
 }
